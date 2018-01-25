cd4e49acd9a854e73e5492f65aead1c4966953b3
Jordan Hoover <jordan@hoovermld.com>
2018-01-25 16:42:54 -0600

    Completed short-url JSON setup

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
-from flask import Flask
-from flask import render_template
+import json
 from datetime import datetime
 
+from flask import Flask, g, render_template, request
+
+import models
+
+DEBUG = True
+
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+@app.before_request
+def before_request():
+	"""Connecting to peewee db"""
+	g.db = models.DATABASE
+	g.db.connect()
+
+@app.after_request
+def after_request(response):
+	"""Disconnecting from peewee db"""
+	g.db.close()
+	return response
 
 @app.route('/')
 def index():
@@ -36,11 +54,46 @@ def time_api(timestamp):
 			data['unix'] = None
 			data['natural'] = None
 
-	return render_template('timestamp.html', timestamp_data=data)
+	return render_template('timestamp.html', timestamp_data=json.dumps(data))
 
 @app.route('/request-parse')
 def request_header_api():
 	return render_template('request-parse.html')
 
+def get_url(long_url):
+	try:
+		a = models.Urls.get(models.Urls.original_url == long_url)
+		return a
+	except models.DoesNotExist:
+		return None
+
+def create_url(long_url):
+	new_url_entry = models.Urls(original_url=long_url)
+	new_url_entry.save()
+
+	return new_url_entry.shortened_url
+
+@app.route('/short-url/<long_url>')
+def url_shortener(long_url):
+	short_url = ''
+
+	if get_url(long_url) == None:
+		short_url = create_url(long_url)
+	else:
+		data_obj = get_url(long_url)
+		short_url = data_obj._data['shortened_url']
+
+	url_dict = {
+		 "original_url": long_url,
+		 "short_url": request.url_root + 'r/' + short_url
+	}
+
+	return render_template('url-shortener.html', url_data=json.dumps(url_dict))
+
+@app.route('/short-url/r/<long_url>')
+def redirect_url():
+	pass
+
 if __name__ == '__main__':
-    app.run(debug=True)
+	models.initialize()
+	app.run(debug=DEBUG)
--- models.py
+++ models.py
@@ -1,13 +1,24 @@
 from datetime import datetime
+import string
+import random
 
 from peewee import *
 
 DATABASE = SqliteDatabase('sql.db')
 
 class Urls(Model):
-    original_url = Charfield()
-    shortened_url = Charfield(unique=True)
+    def unique_string_generator(size=6):
+        return ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(size))
+
+    original_url = CharField()
+    shortened_url = CharField(unique=True, default=unique_string_generator())
     created_at = DateTimeField(default=datetime.now)
 
     class Meta:
         database = DATABASE
+
+def initialize():
+    import pdb; pdb.set_trace
+    DATABASE.connect()
+    DATABASE.create_tables([Urls], safe=True)
+    DATABASE.close()
